14965233bc7476a2001a0842aaa993c0cf535bbb
M Dani Saputra <danniisaputra05@gmail.com>
2025-09-03 09:54:17 +0700

    first commit

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $permissions = [
+            'view courses',
+            'create courses',
+            'edit courses',
+            'delete courses',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $teacherRole = Role::create(['name' => 'teacher']);
+        $teacherRole->givePermissionTo([
+            'view courses',
+            'create courses',
+            'edit courses',
+            'delete courses',
+        ]);
+
+        $studentRole = Role::create(['name' => 'student']);
+        $studentRole->givePermissionTo([
+            'view courses'
+        ]);
+
+        // create user superadmin
+        $user = User::create([
+            'name' => 'Bu Guru',
+            'email' => 'superadmin@teacher.com',
+            'password' => bcrypt('teacher123123'),
+        ]);
+        $user->assignRole($teacherRole);
+    }
+
+}
