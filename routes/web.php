e1d3f2f166c5ad569c2c4f999c84a2cb8dd7381a
M Dani Saputra <danniisaputra05@gmail.com>
2025-09-07 11:19:17 +0700

    routing preparation

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CourseQuestionController;
+use App\Http\Controllers\CourseStudentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\LearningController;
+use App\Http\Controllers\StudentAnswerController;
+use Spatie\Permission\Contracts\Role;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,9 +24,52 @@ Route::middleware('auth')->group(function () {
 
     // routing for course
     Route::prefix('dashboard')->name('dashboard.')->group(function () {
-        Route::resource('courses', CourseController::class)->middleware('role:teacher'); // only for teacher
+        Route::resource('courses', CourseController::class)
+        ->middleware('role:teacher'); // only for teacher
 
-        Route::get('/learning', [LearningController::class, 'index'])->middleware('role:student')->name('learning.index');
+        Route::get('courses/question/create/{course}', [CourseQuestionController::class], 'create')
+        ->middleware('role:teacher')
+        ->name('course.create.question'); // form create question
+
+        Route::post('courses/question/save/{course}', [CourseQuestionController::class], 'store')
+        ->middleware('role:teacher')
+        ->name('course.create.question.store'); // save data question
+
+        Route::resource('courses_questions', CourseQuestionController::class)
+        ->middleware('role:teacher');
+
+        Route::get('/courses/students/show/{course}', [CourseStudentController::class, 'index'])
+        ->middleware('role:teacher')
+        ->name('course.course_students.index'); // view data student
+
+        Route::get('/courses/students/create/{course}', [CourseStudentController::class, 'create'])
+        ->middleware('role:teacher')
+        ->name('course.course_students.create'); // form create student
+
+        Route::get('/courses/students/save/{course}', [CourseStudentController::class, 'store'])
+        ->middleware('role:teacher')
+        ->name('course.course_students.store'); // save data student
+
+        Route::get('/learning/finished/{course}', [LearningController::class, 'learning_finished'])
+        ->middleware('role:student')
+        ->name('learning.finished.course'); // view data learning finished for student
+
+        Route::get('/learning/rapport/{course}', [LearningController::class, 'learning_rapport'])
+        ->middleware('role:student')
+        ->name('learning.rapport.course'); // view data rapport student
+
+        Route::get('/learning', [LearningController::class, 'index'])
+        ->middleware('role:student')
+        ->name('learning.index'); // view student class courses assigne by teacher
+
+        Route::get('/learning/{course}/{question}', [LearningController::class, 'learning'])
+        ->middleware('role:student')
+        ->name('learning.course'); // view progress learning student
+
+        Route::post('/learning/{course}/{question}', [StudentAnswerController::class, 'store'])
+        ->middleware('role:student')
+        ->name('learning.course.answer.store'); // save data answer student
+
     });
 });
 
