c1f129e2c0b26591785608e24880a421e8e2d336
M Dani Saputra <danniisaputra05@gmail.com>
2025-09-07 10:58:26 +0700

    setup routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LearningController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // routing for course
+    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+        Route::resource('courses', CourseController::class)->middleware('role:teacher'); // only for teacher
+
+        Route::get('/learning', [LearningController::class, 'index'])->middleware('role:student')->name('learning.index');
+    });
 });
 
 require __DIR__.'/auth.php';
