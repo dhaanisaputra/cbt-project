ff9550aa4d1b505ea920218f809b7b49994d5332
M Dani Saputra <danniisaputra05@gmail.com>
2025-09-10 10:46:30 +0700

    view course student

--- app/Http/Controllers/LearningController.php
+++ app/Http/Controllers/LearningController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LearningController extends Controller
 {
     public function index()
     {
-        return view('students.courses.index');
+        $user = Auth::user();
+        $my_courses = $user->courses()->with('category')->orderBy('id', 'DESC')->get();
+        return view('students.courses.index', [
+            'my_courses' => $my_courses
+        ]);
     }
 }
