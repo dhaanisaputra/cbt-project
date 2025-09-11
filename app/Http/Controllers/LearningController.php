c5e58f63f6ea19e9c91a6372c6c51bab02742117
M Dani Saputra <danniisaputra05@gmail.com>
2025-09-11 13:34:11 +0700

    view rapport student

--- app/Http/Controllers/LearningController.php
+++ app/Http/Controllers/LearningController.php
@@ -55,4 +55,33 @@ class LearningController extends Controller
             'question' => $currentQuestion
         ]);
     }
+
+    public function learning_finished(Course $course) {
+        $userId = Auth::user()->id;
+
+        return view('students.courses.learning_finish', [
+            'course' => $course,
+        ]);
+    }
+
+    public function learning_rapport(Course $course) {
+        $userId = Auth::user()->id;
+
+        $studentAnswers = StudentAnswer::with('question')
+        ->whereHas('question', function ($query) use ($course) {
+            $query->where('course_id', $course->id);
+        })->where('user_id', $userId)->get();
+
+        $totalQuestions = CourseQuestion::where('course_id', $course->id)->count();
+        $correctAnswersCount = $studentAnswers->where('answer', 'correct')->count();
+        $passed = $correctAnswersCount === $totalQuestions;
+
+        return view('students.courses.learning_rapport', [
+            'course' => $course,
+            'passed' => $passed,
+            'studentAnswers' => $studentAnswers,
+            'totalQuestions' => $totalQuestions,
+            'correctAnswersCount' => $correctAnswersCount,
+        ]);
+    }
 }
